836d7fcd8c350cc41204c542513eb99dffd2491a
fotedarsurbhi <fotedarsurbhi1010@gmail.com>
2019-05-26 00:00:16 +0530

    Drag and Drop functionality in same department working

---
+++ js-coding-assignment/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { SharedModule } from './shared/shared.module';
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompanyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ js-coding-assignment/src/app/company/company.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.scss']
+})
+export class CompanyComponent implements OnInit {
+
+  dataSource: any=[
+    {departmentName: "Frontend", empName : [ "Rohini","Mohini","Sarojini","Bala","Venkat" ]},
+    {departmentName:"Operations",empName : [ "Sriram","Nitin","Raghav","Preeti","Vijay" ]},
+    {departmentName:"Finance",empName : [ "Vishnu","Vardhan","Ganesh","Dinesh","Raju" ]},
+    {departmentName: "Business Developmet" ,empName : [ "Aditya","Senthil","Kavipriya","john","Happy" ]},
+    {departmentName:"Backend" ,empName: [ "Jitendra","Pradeep","Yoganandh","Heena","Rohit" ]},
+    {departmentName:"QA" ,empName: [ "Maitri","Vaishnavi","Sushmita","Sadik","Sreenivas" ]}
+ ];
+ list: any[];
+
+  constructor() {
+   }
+
+  ngOnInit() {
+    console.log(this.dataSource);
+  }
+
+  drop(event: CdkDragDrop<string[]>,list) {
+    // this.dataSource.forEach((data)=>{
+    //   this.list = [];
+    //   this.list.push(data.empName);
+    //   console.log(this.list);
+    // });
+    moveItemInArray(list, event.previousIndex, event.currentIndex);
+     }
+}
