fc9a7d807db5f373182abdf0534f525c89f4f2df
fotedarsurbhi <fotedarsurbhi1010@gmail.com>
2019-05-26 14:40:37 +0530

    Drag and Drop functionality between different departments working

--- js-coding-assignment/src/app/company/company.component.ts
+++ js-coding-assignment/src/app/company/company.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-company',
@@ -8,38 +8,41 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 })
 export class CompanyComponent implements OnInit {
 
-  dataSource: any=[
-    {departmentName: "Frontend", empName : [ "Rohini","Mohini","Sarojini","Bala","Venkat" ]},
-    {departmentName:"Operations",empName : [ "Sriram","Nitin","Raghav","Preeti","Vijay" ]},
-    {departmentName:"Finance",empName : [ "Vishnu","Vardhan","Ganesh","Dinesh","Raju" ]},
-    {departmentName: "Business Developmet" ,empName : [ "Aditya","Senthil","Kavipriya","john","Happy" ]},
-    {departmentName:"Backend" ,empName: [ "Jitendra","Pradeep","Yoganandh","Heena","Rohit" ]},
-    {departmentName:"QA" ,empName: [ "Maitri","Vaishnavi","Sushmita","Sadik","Sreenivas" ]}
- ];
- list: any[];
- value: any ='hello';
+  dataSource: any = [];
+  connectedTo: any = [];
+  list: any[];
+  value: any;
   constructor() {
-   }
+    this.dataSource = [
+      { departmentName: "Frontend", empName: ["Rohini", "Mohini", "Sarojini", "Bala", "Venkat"] },
+      { departmentName: "Operations", empName: ["Sriram", "Nitin", "Raghav", "Preeti", "Vijay"] },
+      { departmentName: "Finance", empName: ["Vishnu", "Vardhan", "Ganesh", "Dinesh", "Raju"] },
+      { departmentName: "Business Developmet", empName: ["Aditya", "Senthil", "Kavipriya", "john", "Happy"] },
+      { departmentName: "Backend", empName: ["Jitendra", "Pradeep", "Yoganandh", "Heena", "Rohit"] },
+      { departmentName: "QA", empName: ["Maitri", "Vaishnavi", "Sushmita", "Sadik", "Sreenivas"] }
+    ];
+
+    this.dataSource.forEach((data) => {
+      this.connectedTo.push(data.departmentName);
+    });
+  }
 
   ngOnInit() {
     console.log(this.dataSource);
   }
 
-  drop(event: CdkDragDrop<string[]>,list) {
-    // this.dataSource.forEach((data)=>{
-    //   this.list = [];
-    //   this.list.push(data.empName);
-    //   console.log(this.list);
-    // });
-    if (event.previousContainer !== event.container) {
-      transferArrayItem(event.previousContainer.data,event.container.data,
-      event.previousIndex, event.currentIndex)
-      } else {
-    moveItemInArray(list, event.previousIndex, event.currentIndex);
-      }
-     }
-
-    click() {
-      console.log("hello");
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     }
+  }
+
+  click() {
+    console.log("hello");
+  }
 }
